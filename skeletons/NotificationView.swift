5384124352794512d97273d82e3965a9c925d587
ToshiroSama <alishdjuraev@gmail.com>
2023-01-25 14:53:04 +0500

    NotificationView is finished

--- skeletons/NotificationView.swift
+++ skeletons/NotificationView.swift
@@ -2,6 +2,41 @@ import UIKit
 import SnapKit
 
 class NotificationView: UIView {
+    
+    private let firstView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1.0)
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    private let secondView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1.0)
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    private let thirdView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1.0)
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    private let boxOneView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1.0)
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    private let boxTwoView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1.0)
+        view.layer.cornerRadius = 12
+        return view
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -10,12 +45,48 @@ class NotificationView: UIView {
     }
     
     private func setupUI() {
+        self.addSubview(firstView)
+        self.addSubview(secondView)
+        self.addSubview(thirdView)
+        self.addSubview(boxOneView)
+        self.addSubview(boxTwoView)
         
         self.setConstraints()
     }
     
     private func setConstraints() {
         
+        firstView.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top).offset(16)
+            make.leading.equalTo(self.snp.leading).offset(16)
+            make.size.equalTo(CGSize(width: 80, height: 36))
+        }
+        
+        secondView.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top).offset(16)
+            make.leading.equalTo(self.firstView.snp.trailing).offset(12)
+            make.size.equalTo(CGSize(width: 80, height: 36))
+        }
+        
+        thirdView.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top).offset(16)
+            make.leading.equalTo(self.secondView.snp.trailing).offset(12)
+            make.size.equalTo(CGSize(width: 80, height: 36))
+        }
+        
+        boxOneView.snp.makeConstraints { make in
+            make.top.equalTo(self.secondView.snp.bottom).offset(32)
+            make.leading.equalTo(self.snp.leading).offset(16)
+            make.trailing.equalTo(self.snp.trailing).offset(-16)
+            make.height.equalTo(318)
+        }
+        
+        boxTwoView.snp.makeConstraints { make in
+            make.top.equalTo(self.boxOneView.snp.bottom).offset(24)
+            make.leading.equalTo(self.boxOneView.snp.leading)
+            make.trailing.equalTo(self.boxOneView.snp.trailing)
+            make.height.equalTo(318)
+        }
     }
     
     required init?(coder: NSCoder) {
